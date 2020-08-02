2f927cd39785185f1cb52ec156a6dd33a211e94f
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2020-08-01 17:22:17 -0700

    MUI theme

--- template/src/app/App.js
+++ template/src/app/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { Container } from "@material-ui/core";
-import theme from "../app/theme";
+import theme from "./theme";
 
 export default function App() {
   return (
